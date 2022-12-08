340fe41c97ec090a8285657b02c641dcc0489dd1
lazarostar <nascimento.dev26@gmail.com>
2022-12-08 15:08:35 +0100

    feat: add iteration show/hide

--- src/components/ExperimentModule.js
+++ src/components/ExperimentModule.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { FaLock, FaLockOpen } from 'react-icons/fa'
 import IterationRow from './IterationRow'
+import NewIterationRow from './NewIterationRow'
 
 export default function ExperimentModule ({ id, imList, isLocked, isClosed, onRowClick }) {
+  const [isAddingIteration, setIsAddingIteration] = useState(!imList.length)
+
+  const handleAddIteration = () => {
+    setIsAddingIteration(true)
+  }
+  const handleCancel = () => {
+    if (imList.length) { setIsAddingIteration(false) }
+  }
+  const handleGenerateIteration = () => {
+
+  }
+
   return (
     <div className={`experiment-module ${isClosed && 'closed'} ${imList.length === 0 && 'empty'} ${isLocked && 'locked'}`}>
       <div className='experiment-head' onClick={() => onRowClick(id)}>
@@ -21,12 +34,31 @@ export default function ExperimentModule ({ id, imList, isLocked, isClosed, onRo
               selection={im.selection}
             />
           ))}
+          {
+            isAddingIteration && <NewIterationRow key='__new__' index={imList.length} />
+          }
         </div>
+        {
+          isAddingIteration && (
+            <p className='new-iteration-hint'>
+              To add a new iteration, start typing a prompt or <a href='javascript:void(0)' onClick={handleGenerateIteration}>generate</a> one.
+            </p>
+          )
+        }
       </div>
       <div className='experiment-footer'>
-        <button className='button'>lock</button>
-        <button className='button'>reset</button>
-        <button className='button active'>+ add iteration</button>
+        {isAddingIteration
+          ? (
+            <>
+              <button className='button' onClick={handleCancel}>cancel</button>
+              <button className='button active'>done</button>
+            </>)
+          : (
+            <>
+              <button className='button'>lock</button>
+              <button className='button'>reset</button>
+              <button className='button active' onClick={handleAddIteration}>+ add iteration</button>
+            </>)}
       </div>
     </div>
   )
